1e6fd02d298b285353a79944ab6aea5b0c0a8854
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-03-29 23:08:59 +0200

    Handle oauth error

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,14 @@
 class AuthController < ApplicationController
     def callback
+        error = params["error"]
+        if error != nil
+            msg = "Error: "+error
+            if error == "access_denied"
+                msg = "Canceled login"
+            end
+            redirect_to root_path, notice: msg
+            return
+        end
         code = params["code"]
         if code == nil || code.blank?
             redirect_to root_path, notice: "Missing authentication code"
