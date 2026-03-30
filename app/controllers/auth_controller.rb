de947a27aa8f58d96efdeb2629da4b61b703f573
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-03-30 17:17:44 +0200

    Make token exchange handled by seperate function

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -13,45 +13,48 @@ class AuthController < ApplicationController
         end
         code = params["code"]
         if code == nil || code.blank?
-            redirect_to root_path, notice: "Missing authentication code"
+            redirect_to root_path, notice: "error: Missing authentication code"
             return
         end
 
         # exchange code for token
-        uri = URI.parse("https://hackatime.hackclub.com/oauth/token")
-        data = '{
-            "client_id": "'+ ENV["HACKATIME_UID"]+ '",
-            "client_secret": "'+ ENV["HACKATIME_SECRET"]+ '",
-            "redirect_uri": "' + url_for(only_path: false) + '",
-            "code": "' + params["code"] + '",
-            "grant_type": "authorization_code"
-        }'
-        headers = { 'content-type': "application/json" }
-        res = Net::HTTP.post(uri, data, headers)
+        response = getToken(code)
 
-        puts res.body
-
-        token = ""
-
-        # whether the authentication was unsuccesful
-        error = (not (res.kind_of? Net::HTTPSuccess)) or res.body.include? "error"
-
-        if not error
-            data = JSON.parse(res.body)
-
-            if data == nil or data["access_token"] == nil
-                # mark authentication as unsuccesful
-                error = true
-            else
-                token = data["access_token"]
-            end
-        end
-
-        if error
-            redirect_to root_path, notice: "Invalid authentication code or authentication server was unreachable"
+        if response[:token] == nil
+            redirect_to root_path, notice: "error: Couldn't get token: " + response[:error].to_s
             return
         end
+        token = response[:token]
 
         redirect_to root_path, notice: "token:"+token
     end
+
+    private
+        # exchange a authentication code for a hackatime user token
+        def getToken(code)
+            uri = URI.parse("https://hackatime.hackclub.com/oauth/token")
+            data = '{
+                "client_id": "'+ ENV["HACKATIME_UID"]+ '",
+                "client_secret": "'+ ENV["HACKATIME_SECRET"]+ '",
+                "redirect_uri": "' + url_for(only_path: false) + '",
+                "code": "' + params["code"] + '",
+                "grant_type": "authorization_code"
+            }'
+            headers = { 'content-type': "application/json" }
+            res = Net::HTTP.post(uri, data, headers)
+
+            # whether the authentication was unsuccesful
+            error = (not (res.kind_of? Net::HTTPSuccess)) or res.body.include? "error"
+
+            if error
+                { "error": "Error from hackatime API" }
+            else
+                data = JSON.parse(res.body)
+                if data == nil or data["access_token"] == nil
+                    { "error": "Bad response from hackatime API" }
+                else
+                    { "token": data["access_token"] }
+                end
+            end
+        end
 end
